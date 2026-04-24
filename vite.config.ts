f4618adf771548f703def29389ba2194cb9a510b
eskialexis7 <eskialexis7@gmail.com>
2026-04-25 01:53:25 +0300

    FIXXXX

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // ΑΛΛΑΞΑΜΕ ΑΥΤΗ ΤΗ ΓΡΑΜΜΗ
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  }
 });
